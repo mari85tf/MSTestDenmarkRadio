976aebdc2133aa3cb45dec98b8459efaae697c52
saugat10 <saugatkhatiwada10@gmail.com>
2022-11-10 10:18:48 +0100

    added test for adding music (not working in my machine)

--- MSTestDenmarkRadio/UnitTest1.cs
+++ MSTestDenmarkRadio/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace MSTestDenmarkRadio
     [TestClass]
     public class UnitTest1
     {
-        private static readonly string DriverDirectory = "C:\\WebDrivers\\chromedriver_win32 (2)";
+        private static readonly string DriverDirectory = "C:\\WebDrivers";
         private static IWebDriver _driver;
 
         [ClassInitialize]
@@ -30,19 +30,36 @@ namespace MSTestDenmarkRadio
 
             Assert.AreEqual("Denmark Radio", _driver.Title);
 
-            IWebElement inputElement = _driver.FindElement(By.Id("deleteInput"));
-            inputElement.SendKeys("10");
+            //IWebElement inputElement = _driver.FindElement(By.Id("deleteInput"));
+            //inputElement.SendKeys("10");
 
-            IWebElement button1 = _driver.FindElement(By.Id("deleteButton"));
-            button1.Click();
+            //IWebElement button1 = _driver.FindElement(By.Id("deleteButton"));
+            //button1.Click();
 
-            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10)); // decorator pattern?
-            IWebElement deleteMusic = wait.Until(d => d.FindElement(By.Id("outputDelete")));
+            //WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10)); // decorator pattern?
+            //IWebElement deleteMusic = wait.Until(d => d.FindElement(By.Id("outputDelete")));
 
-            Assert.AreEqual("200", deleteMusic.Text);
+            //Assert.AreEqual("200", deleteMusic.Text);
 
+            IWebElement inputTitle = _driver.FindElement(By.Id("title"));
+            inputTitle.SendKeys("test");
 
+            IWebElement inputArtist = _driver.FindElement(By.Id("artist"));
+            inputArtist.SendKeys("tester");
 
+            IWebElement inputDuration = _driver.FindElement(By.Id("duration"));
+            inputDuration.SendKeys("1");
+
+            IWebElement inputPublicationYear = _driver.FindElement(By.Id("publicationYear"));
+            inputPublicationYear.SendKeys("2000");
+
+            IWebElement button2 = _driver.FindElement(By.Id("addMusic"));
+            button2.Click();
+
+            WebDriverWait waitMusic = new WebDriverWait(_driver, TimeSpan.FromSeconds(10)); // decorator pattern?
+            IWebElement addMusic = waitMusic.Until(d => d.FindElement(By.Id("addMessage")));
+
+            Assert.AreEqual("response 201", addMusic.Text);
         }
     }
 }
